c6bb12251e9cda1b969c8f4f86fa8f9894ba2e9e
jialiya <jialiya.huang@students.olin.edu>
2014-06-09 16:25:59 -0700

    cleaning out rpi deps

--- test-details.js
+++
@@ -1,9 +0,0 @@
-var NRF24 = require("./index"),
-    spiDev = process.argv[2] || "/dev/spidev0.0",
-    cePin = +process.argv[3] || 24,
-    irqPin = +process.argv[4] || 25;
-
-var nrf = (irqPin > 0) ?
-    NRF24.connect(spiDev, cePin, irqPin) :
-    NRF24.connect(spiDev, cePin);
-nrf.printDetails();
